2b9426e7fc7503aba8cd101cbef709a2310c51b5
Tenosiey <tjark.froelje@gmail.com>
2021-08-11 14:45:17 +0200

    Added the Dealers Hand

--- blackjack.py
+++ blackjack.py
@@ -5,7 +5,7 @@ def blackjack_init():
 
     shoe = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4*6         # Creating the shoe with 6 seperate decks with 13*4 cards
 
-    def deal(shoe):
+    def deal_player(shoe):
         player_hand = []
         for i in range(2):
             random.shuffle(shoe)
@@ -22,6 +22,23 @@ def blackjack_init():
 
         return player_hand
 
+    def deal_dealer(shoe):
+        dealer_hand = []
+        for i in range(2):
+            random.shuffle(shoe)
+            card = shoe.pop()
+            if card == 11:
+                card = "J"
+            if card == 12:
+                card = "Q"
+            if card == 13:
+                card = "K"
+            if card == 14:
+                card = "A"
+            dealer_hand.append(card)
+
+        return dealer_hand
+
     def player_total(player_hand):
         player_total = 0
         
@@ -35,11 +52,29 @@ def blackjack_init():
             else:
                 player_total += card
 
-        print("Current Hand is: " + str(player_hand))
-        print("Current Total is: " + str(player_total))
+        print("Current Player Hand is: " + str(player_hand))
+        print("Current Player Total is: " + str(player_total))
         return player_total
+
+    def dealer_total(dealer_hand):
+        dealer_total = 0
+        
+        for card in dealer_hand:
+            if card == "J" or card == "Q" or card == "K":
+                dealer_total += 10
+            elif card == "A" and dealer_total >= 11:
+                dealer_total += 1
+            elif card == "A" and dealer_total <= 10:
+                dealer_total += 11
+            else:
+                dealer_total += card
+
+        print("Current Dealer Hand is: " + str(dealer_hand))
+        print("Current Dealer Total is: " + str(dealer_total))
+        return dealer_total
     
-    player_total(deal(shoe))
+    player_total(deal_player(shoe))
+    dealer_total(deal_dealer(shoe))
 
 if __name__ == "__main__":
     blackjack_init()
