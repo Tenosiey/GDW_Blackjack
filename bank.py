390220a45f8a255fce3b14363d5794cdbb7120fc
Tenosiey <tjark.froelje@gmail.com>
2021-08-12 14:32:11 +0200

    user cant register double

--- bank.py
+++ bank.py
@@ -1,19 +1,26 @@
 import json
 
 def register(discord_uid, username, user_id):
-    data = {}
-    data["user"] = []
-    data["user"].append({
-        "discord_uid": str(discord_uid),
-        "username": username,
-        "user_id": user_id,
-        "money": 0,
-        "games_played": 0,
-        "games_won": 0
-    })
+    with open("bank.json") as jsonFile:
+        a_dictionary = json.load(jsonFile)
+        jsonFile.close()
 
-    with open("bank.json", "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
+    if str(discord_uid) in a_dictionary:
+        print("User is already registered")
+    else:
+        data = {}
+        data[str(discord_uid)] = []
+        data[str(discord_uid)].append({
+            "discord_uid": str(discord_uid),
+            "username": username,
+            "user_id": user_id,
+            "money": 0,
+            "games_played": 0,
+            "games_won": 0
+        })
+
+        with open("bank.json", "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     register()
