efe9c08e50f8e9e7caa8e721ee1069d640b364d6
Tenosiey <tjark.froelje@gmail.com>
2021-08-12 09:42:29 +0200

    Rewrote the complete game, and added User

--- blackjack.py
+++ blackjack.py
@@ -1,80 +1,83 @@
 import random
 
-def blackjack_init():    
-    game_count = 0
+def deal():
+    hand = []
 
-    shoe = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4*6         # Creating the shoe with 6 seperate decks with 13*4 cards
+    for i in range(2):
+        card = shoe.pop()
+        if card == 1:
+            card = "A"
+        if card == 11:
+            card = "J"
+        if card == 12:
+            card = "Q"
+        if card == 13:
+            card = "K"
+        hand.append(card)
+    return hand
 
-    def deal_player(shoe):
-        player_hand = []
-        for i in range(2):
-            random.shuffle(shoe)
-            card = shoe.pop()
-            if card == 11:
-                card = "J"
-            if card == 12:
-                card = "Q"
-            if card == 13:
-                card = "K"
-            if card == 14:
-                card = "A"
-            player_hand.append(card)
+def total(hand):
+    total = 0
+    for card in hand:
+        if card == "A" and total >= 11:
+            total += 1
+        elif card == "A" and total <= 10:
+            total += 11
+        elif card == "J" or card == "Q" or card == "K":
+            total += 10
+        else:
+            total += card
+    return total
 
-        return player_hand
+def blackjack():
+    print("Player got a Blackjack")
 
-    def deal_dealer(shoe):
-        dealer_hand = []
-        for i in range(2):
-            random.shuffle(shoe)
-            card = shoe.pop()
-            if card == 11:
-                card = "J"
-            if card == 12:
-                card = "Q"
-            if card == 13:
-                card = "K"
-            if card == 14:
-                card = "A"
-            dealer_hand.append(card)
+def play_split():
+    input("Would you like to split your cards? y/n")
 
-        return dealer_hand
+def stand():
+    print("Ok, stand")
 
-    def player_total(player_hand):
-        player_total = 0
-        
-        for card in player_hand:
-            if card == "J" or card == "Q" or card == "K":
-                player_total += 10
-            elif card == "A" and player_total >= 11:
-                player_total += 1
-            elif card == "A" and player_total <= 10:
-                player_total += 11
-            else:
-                player_total += card
+def hit():
+    print("Ok, hit")
 
-        print("Current Player Hand is: " + str(player_hand))
-        print("Current Player Total is: " + str(player_total))
-        return player_total
+def double():
+    print("Ok, double")
 
-    def dealer_total(dealer_hand):
-        dealer_total = 0
-        
-        for card in dealer_hand:
-            if card == "J" or card == "Q" or card == "K":
-                dealer_total += 10
-            elif card == "A" and dealer_total >= 11:
-                dealer_total += 1
-            elif card == "A" and dealer_total <= 10:
-                dealer_total += 11
-            else:
-                dealer_total += card
+def play():
+    choice = input("Would you like to Hit, Stand or Double?")
+    if choice == "stand":
+        stand()
+    elif choice == "hit":
+        hit()
+    elif choice == "double":
+        double()
+    else:
+        print("Error, command not known")
 
-        print("Current Dealer Hand is: " + str(dealer_hand))
-        print("Current Dealer Total is: " + str(dealer_total))
-        return dealer_total
+def game():
+    dealer_hand = deal()
+    player_hand = deal()
+
+    dealer_total_var = total(dealer_hand)
+    player_total_var = total(player_hand)
+
+    print("Dealers hand is: " + str(dealer_hand))
+    print("Dealers total is: " + str(dealer_total_var))
     
-    player_total(deal_player(shoe))
-    dealer_total(deal_dealer(shoe))
+    print("Players hand is: " + str(player_hand))
+    print("Players total is: " + str(player_total_var))
+
+    if player_total_var == 21:
+        blackjack()
+    elif player_hand[0] == player_hand[-1]:
+        play_split()
+    else:
+        play()
 
 if __name__ == "__main__":
-    blackjack_init()
+    decks = 6                                                               # Setting the size of the shoe
+    shoe = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]*4*decks              # Creating the shoe
+    random.shuffle(shoe)                                                    # Shuffle the shoe (5 times for good meassure)
+
+    game()
