66ba48b0973647a7095996a0a5380046b444f8d3
Tenosiey <tjark.froelje@gmail.com>
2021-08-17 13:07:43 +0200

    added a $help function

--- main.py
+++ main.py
@@ -26,12 +26,19 @@ def main():
         if message.author == client.user:
             return
 
+        if message.content == "$help":
+            bot_help = open("help.txt", "r")
+            await message.channel.send("```" + bot_help.read() + "```")
+
         if message.content == "$register":
             bank.register(message.author, message.author.name, message.author.id)
 
         if message.content == "$money":
             money = bank.money(message.author)
             await message.channel.send("<@" + str(message.author.id) + ">\nCurrently you have: " + str(money) + "$")
+
+        if message.content == "$blackjack":
+            blackjack.play()
 
     client.run(TOKEN)
 
