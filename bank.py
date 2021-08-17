cc6ffa33ef526637cf9ba55f70e483ece4ebb0ae
Tenosiey <tjark.froelje@gmail.com>
2021-08-17 12:04:47 +0200

    test

--- bank.py
+++ bank.py
@@ -12,6 +12,7 @@ def register(discord_uid, username, user_id):
             "discord_uid": str(discord_uid),
             "username": username,
             "user_id": user_id,
+            "rank": "player",
             "money": 1000,
             "games_played": 0,
             "games_won": 0
